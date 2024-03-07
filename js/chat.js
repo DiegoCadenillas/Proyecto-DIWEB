5becf411354e883bcc6dd61476aad1cdf4b233d1
DiegoCadenillas <dabonkinator845@gmail.com>
2024-03-07 22:00:27 +0100

    Últimos retoques (real).

--- js/chat.js
+++ js/chat.js
@@ -1,6 +1,10 @@
 function enviarMensaje() {
     let mensajes = document.getElementById("mensajes");
+    let mensajesActuales = mensajes.children;
     let mensaje = document.getElementById("mensaje");
-    mensajes.innerHTML += `<p><b>Usuario:</b> ${mensaje.value}</p>`;
-    mensaje.value = '';
+    if (mensaje.value != "") {
+        if (mensajesActuales[0].id == "mensaje-inicial") mensajes.innerHTML = "";
+        mensajes.innerHTML += `<p><b>Usuario:</b> ${mensaje.value}</p>`;
+        mensaje.value = '';
+    }
 }
